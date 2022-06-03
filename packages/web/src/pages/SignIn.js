db2e81212ea6319149360eaf3d78469115d3f66b
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-06-03 10:22:16 -0300

    Aula fazendo requisicao assincrona

--- packages/web/src/pages/SignIn.js
+++ packages/web/src/pages/SignIn.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SignIn() {
+    const [email, setEmail] = useState('');
+    const [passaword, setPassaword] = useState('');
+
+    const handleEmailChange = (event) => setEmail(event.target.value);
+    const handlePassawordChange = (event) => setPassaword(event.target.value);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://127.0.0.1:8000/authenticate', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                passaword,
+            }),
+        }).then((response) => response.json())
+          .then(() => console.log('Success!'));
+    };
+
     return (
-        <form action="/authenticate" method="post">
+        <form onSubmit={handleSubmit}>
             <fieldset>
-                <label for="email">E-mail</label>
-                <input id="email" name="email" type="email" inputmode="email" autocomplete="username"/>
+                <label htmlFor="email">E-mail</label>
+                <input 
+                    id="email" 
+                    value={email} 
+                    onChange={handleEmailChange}
+                    type="email" 
+                    inputmode="email" 
+                    autoComplete="username"/>
             </fieldset>
             <fieldset>
-                <label for="password">Senha</label>
-                <input id="password" name="password" type="password" autocomplete="current-password"/>
+                <label htmlFor="password">Senha</label>
+                <input 
+                    id="password" 
+                    value={password}
+                    onChange={handlePassawordChange} 
+                    type="password" 
+                    autoComplete="current-password"/>
             </fieldset>
             <button type="submit">Entrar</button>
         </form>
